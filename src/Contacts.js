b5c18d539954fdbf5032c97aa669892727f795b0
Behnood <behnoodii@gmail.com>
2019-05-03 22:14:06 +0430

    Modify codebase

--- src/Contacts.js
+++ src/Contacts.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Chat from './Chat';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './Contacts.css';
 
 export default class Contacts extends Component {
 
